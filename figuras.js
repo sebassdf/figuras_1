7d97e2ccc703e504a592496d0270fed918c26321
sebassdf <se.nunezm@duocuc.cl>
2024-06-03 00:34:16 -0400

    figuras2 para render

--- figuras.js
+++ figuras.js
@@ -3,26 +3,83 @@
 const figuras = [
     {
         "id":1,
-        "nombre":"figura 1",
-        "valor":"1000"
+        "nombre":"Spider-Man Advanced Suit",
+        "descripción":"Pop Culture Shock Collectibles presenta a Spider-Man Advanced Suit, en escala 1:3, de edición limitada, basada en el popular videojuego de PS4.",
+        "categoria":"Escala 1:3, Figuras Cine y TV, Figuras de Comics, Figuras de Video Juegos, Marvel",
+        "valor":"$1.099.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2019/10/spider-man-advanced-suit_marvel_gallery_5da64b95520a2.jpg"
     },
 
     {
         "id":2,
-        "nombre":"figura 2",
-        "valor":"2000"
+        "nombre":"Ryu",
+        "descripción":"Iconiq Studios presenta a Ryu, en escala 1/6 de edición limitada. La figura está especialmente diseñada sobre la base de Ryu en el Videojuego Street Fighter V Champion Edition.",
+        "categoria":"Figuras 1:6, Figuras de Video Juegos",
+        "valor":"$379.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2021/10/ryu_street-fighter_gallery_6155ee2595880.jpg"
     },
 
     {
         "id":3,
-        "nombre":"figura 3",
-        "valor":"3000"
+        "nombre":"Chun-Li",
+        "descripción":"Pop Culture Shock presenta  a Chun-Li de su linea de estatuas en formato 1/4 de edición limitada, basadas en el popular videojuego Street Fighter.",
+        "categoria":"Figuras 1:4, Figuras de Video Juegos, SDCC & NYCC Exclusivos",
+        "valor":"$649.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2020/04/chun-li_street-fighter_gallery_5e8e6c12df74c.jpg"
     },
 
     {
         "id":4,
-        "nombre":"figura 4",
-        "valor":"4000"
+        "nombre":"Batman The Grim Knight",
+        "descripción":"McFarlane Toys presenta su linea de figuras de 7″, basadas en los personajes de DC Comics. Figuras diseñadas con detalles de calidad y totalmente articuladas.",
+        "categoria":"DC Comics, Escala 1:10, Figuras de Comics, Figuras de Video Juegos",
+        "valor":"$27.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2021/03/the_grim-mcfarlane.jpg"
+    },
+
+    {
+        "id": 5,
+        "nombre":"Captain America",
+        "descripción":"Hot Toys presenta al Capitan America en escala 1/6 de edición limitada en la línea Movie Masterpiece de Marvel.",
+        "categoria":"Figuras 1:6, Figuras Cine y TV, Hot Toys, Marvel",
+        "valor":"$349.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2021/12/cap4.jpg"
+    },
+
+    {
+        "id": 6,
+        "nombre":"The Bad Batch Hunter",
+        "descripción":"Hot Toys presenta a Hunter, en escala 1/6 de edición limitada en la línea TV Masterpiece. La figura está especialmente diseñada sobre la base de Echo, en la nueva serie de Star Wars.",
+        "categoria":"Figuras 1:6, Figuras Cine y TV, Hot Toys, Star Wars",
+        "valor":"$279.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2021/06/198055046_10158073623522344_6629860458965178360_n.jpg"
+    },
+
+    {
+        "id": 7,
+        "nombre":"Yoda",
+        "descripción":"Sideshow Collectibles presenta a Yoda, en escala 1/6 de edición limitada. La figura está especialmente diseñada sobre la base del Maestro Yoda en la serie animada Star Wars.",
+        "categoria":"Figuras 1:6, Figuras Cine y TV, Sideshow, Star Wars",
+        "valor":"$179.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2022/04/yoda_star-wars_gallery_6244fb6fce244.jpg"
+    },
+   
+    {
+        "id": 8,
+        "nombre":"The Joker",
+        "descripción":"La compañía Iron Studios presenta su línea de estatuas en escala 1:10, Art Scale 1/10, de DC Comics, basadas en la película Batman Returns, fabricadas en polystone y pintadas a mano con un detalle y calidad premium.",
+        "categoria":"DC Comics, Escala 1:10, Figuras Cine y TV, Figuras de Comics, Iron Studios",
+        "valor":"$129.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2021/06/174092562_4236445496367153_3381872822803838955_n.jpg",
+    },
+
+    {
+        "id": 9,
+        "nombre":"Venom",
+        "descripción":"Sideshow Collectibles presenta su línea Premium Format. Estatuas de formato escala 1/4, fabricadas en Polystone, con un impresionante realismo, un detalle y acabado único, una estatua de nivel Premium.",
+        "categoria":"Figuras 1:4, Figuras de Comics, Marvel, Sideshow",
+        "valor":"$749.990",
+        "imagen": "https://www.kametoys.cl/wp-content/uploads/2022/01/venom_marvel_gallery_61e0c4d6c472e.jpg"
     },
 ];
 
